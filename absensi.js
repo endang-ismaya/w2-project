69e5d1fa52285e88facb15ab54b101682be704b2
endang.ismaya <endang.ismaya@gmail.com>
2018-11-23 06:22:58 +0000

    add showAbsensiSiswa(type) with single-join, menambahkan title pada tiap eksekusi di output

--- absensi.js
+++ absensi.js
@@ -71,6 +71,9 @@ var absensi_siswa = [
 ];
 
 // menampilkan data absensi siswa awal
+console.log('#-----------------------------'); // for title
+console.log('# Informasi Data Absensi Awal'); // for title
+console.log('#-----------------------------'); // for title
 console.log(absensi_siswa);
 
 // Function untuk mendapatkan data lengkap siswa dari no_induk
@@ -112,11 +115,11 @@ function addAbsensiSiswa(tanggal, noInduk, absen) {
     const dataSiswa = getDetailSiswa(noInduk);
 
     if (dataSiswa[0] === '00000') {
-        console.log('\n~Tambah data absen gagal, invalid no-induk siswa~\n');
+        console.log('~Tambah data absen gagal, invalid no-induk siswa~\n');
     } else  {
         let arrData = [tanggal, dataSiswa[0], dataSiswa[1], dataSiswa[2], absen];
         absensi_siswa.push(arrData);
-        console.log('\n~Tambah data absen berhasil.~\n');
+        console.log('~Tambah data absen berhasil.~\n');
     }
 }
 
@@ -134,14 +137,14 @@ function deleteAbsensiSiswa(tanggal, noInduk) {
 
         if (tanggal === tanggalAbsensi && noInduk === noIndukAbsensi) {
             absensi_siswa.splice(i, 1);
-            console.log('\n~Data absen berhasil dihapus!.~\n');
+            console.log('~Data absen berhasil dihapus!.~\n');
             isBerhasilDiHapus = true;
             break;
         }
     }
 
     if (!isBerhasilDiHapus) {
-        console.log('\n~Data absen gagal dihapus, tolong cek inputan kembali!.~\n');
+        console.log('~Data absen gagal dihapus, tolong cek inputan kembali!.~\n');
     }
 }
 
@@ -152,9 +155,9 @@ function deleteLastAbsensiSiswa() {
 
     if (absensi_siswa.length > 0) {
         absensi_siswa.pop();
-        console.log('\n~Data absen terakhir berhasil dihapus!.~\n');
+        console.log('~Data absen terakhir berhasil dihapus!.~\n');
     } else {
-        console.log('\n~Tidak Ada yang terhapus!~.');
+        console.log('~Tidak Ada yang terhapus!~.');
     }
 }
 
@@ -165,9 +168,9 @@ function deleteFirstAbsensiSiswa() {
 
     if (absensi_siswa.length > 0) {
         absensi_siswa.shift();
-        console.log('\n~Data absen pertama berhasil dihapus!.~\n');
+        console.log('~Data absen pertama berhasil dihapus!.~\n');
     } else {
-        console.log('\n~Tidak Ada yang terhapus!~.');
+        console.log('~Tidak Ada yang terhapus!~.');
     }
 }
 
@@ -188,7 +191,7 @@ function getDetailAbsensi(kodeAbsensi) {
 
 // Function untuk menampilkan data absensi siswa
 // Berdasarkan input Hari, Tanggal dan no-induk siswa
-// Menggunakan: split, switch
+// Menggunakan: split, switch, join
 // Return: Function dari type menampilkan siswa berdasarkan type tampilan
 function showAbsensiSiswa(type) {
 
@@ -220,6 +223,22 @@ function showAbsensiSiswa(type) {
 
                 return result;
             };
+        case 'single-join':
+            return function(tanggal, noInduk) {
+                var result = '~Data tidak ditemukan!~';
+
+                for (let i = 0; i < absensi_siswa.length; i++) {
+                    const dataSiswa = absensi_siswa[i];
+                    const hariTanggal = dataSiswa[0];
+                    const noIndukSiswa = dataSiswa[1];
+
+                    if (tanggal === hariTanggal && noInduk === noIndukSiswa) {
+                        result = dataSiswa.join(' | ');
+                    }
+                }
+
+                return result;
+            };
 
         default:
             break;
@@ -228,16 +247,50 @@ function showAbsensiSiswa(type) {
 
 // Function untuk mengedit data absensi siswa
 // Berdasarkan Hari, Tanggal dan no-induk siswa
+// Menampilkan value before update
+// Dan Menampilkan value after update
+function updateAbsensiSiswa(tanggal, noInduk, absensi) {
+    var result = '~Data tidak ditemukan!~';
 
+    for (let i = 0; i < absensi_siswa.length; i++) {
+        const dataSiswa = absensi_siswa[i];
+        const hariTanggal = dataSiswa[0];
+        const noIndukSiswa = dataSiswa[1];
+
+        if (tanggal === hariTanggal && noInduk === noIndukSiswa) {
+
+            // Before update
+            let singleJoinShowBefore = showAbsensiSiswa('single-join');
+            let dataSingleJoinBefore = singleJoinShowBefore(tanggal, noInduk);
+            result = 'BEFORE: ' + dataSingleJoinBefore + "\n";
+
+            // updating
+            absensi_siswa[i][4] = absensi;
+
+            // After Update
+            let singleJoinShowAfter = showAbsensiSiswa('single-join');
+            let dataSingleJoinAfter = singleJoinShowAfter(tanggal, noInduk);
+            result += 'AFTER : ' + dataSingleJoinAfter + "\n";
+        }
+    }
+
+    return result;
+}
 
 /*
     Test Tambah data Absensi
 */
 // test add absensi dengan data invalid -> noinduk yg tidak ada di data '1A004'
+console.log('\n#-------------------------------------------'); // for title
+console.log('# Tambah Data Absensi dengan invalid input'); // for title
+console.log('#-------------------------------------------'); // for title
 addAbsensiSiswa('Rabu, 07-11-2018', '1A004', 'A');
 console.log(absensi_siswa);
 
 // test add absensi dengan data valid -> noinduk ada di data '1A003'
+console.log('\n#-------------------------------------------'); // for title
+console.log('# Tambah Data Absensi dengan valid input'); // for title
+console.log('#-------------------------------------------'); // for title
 addAbsensiSiswa('Rabu, 07-11-2018', '1B002', 'A');
 addAbsensiSiswa('Rabu, 07-11-2018', '1C003', 'I');
 console.log(absensi_siswa);
@@ -248,26 +301,64 @@ console.log(absensi_siswa);
 
 // test delete data absensi dengan invalid data
 // 'Senin, 12-11-2018', '1C003'
+console.log('\n#-------------------------------------------'); // for title
+console.log('# Hapus Data Absensi dengan invalid input'); // for title
+console.log('#-------------------------------------------'); // for title
 deleteAbsensiSiswa('Senin, 12-11-2018', '1C003');
 console.log(absensi_siswa);
 
 // test delete data absensi dengan valid data
 // 'Senin, 05-11-2018', '1C003'
+console.log('\n#-------------------------------------------'); // for title
+console.log('# Hapus Data Absensi dengan valid input'); // for title
+console.log('#-------------------------------------------'); // for title
 deleteAbsensiSiswa('Senin, 05-11-2018', '1C003');
 console.log(absensi_siswa);
 
 // test delete data absensi terakhir
+console.log('\n#-------------------------------------------'); // for title
+console.log('# Hapus Data Absensi dengan input terakhir'); // for title
+console.log('#-------------------------------------------'); // for title
 deleteLastAbsensiSiswa();
 console.log(absensi_siswa);
 
 // test delete data absensi pertama
+console.log('\n#-------------------------------------------'); // for title
+console.log('# Hapus Data Absensi dengan input pertama'); // for title
+console.log('#-------------------------------------------'); // for title
 deleteFirstAbsensiSiswa();
 console.log(absensi_siswa);
+console.log('\n'); // for indentation
 
 // test tampilkan data siswa
 var singleShow = showAbsensiSiswa('single');
-var dataSingleShow = singleShow('Selasa, 06-11-2018', '1A001'); // data valid
-console.log(dataSingleShow);
+var dataSingle = singleShow('Selasa, 06-11-2018', '1A001'); // data valid
+console.log('#------------------------'); // for title
+console.log('# Informasi Data Detail'); // for title
+console.log('#------------------------'); // for title
+console.log(dataSingle);
 
 var dataSingleShow = singleShow('Selasa, 06-11-2018', '1K001'); // data invalid
+console.log('#---------------------------------------------'); // for title
+console.log('# Informasi Data Detail dengan invalid input'); // for title
+console.log('#---------------------------------------------'); // for title
 console.log(dataSingleShow);
+console.log('\n'); // for indentation
+
+var singleJoinShow = showAbsensiSiswa('single-join');
+var dataSingleJoin = singleJoinShow('Selasa, 06-11-2018', '1A001'); // data valid
+console.log('#--------------------'); // for title
+console.log('# Informasi Data Join'); // for title
+console.log('#--------------------'); // for title
+console.log(dataSingleJoin);
+console.log(); // for indentation
+
+
+// test update data absensi siswa
+//  'Senin, 05-11-2018', '1B001', 'Masruq', '1A', 'H' -> Hadir
+// jadi:  'Senin, 05-11-2018', '1B001', 'Masruq', '1A', 'S' -> Sakit
+console.log('#--------------------'); // for title
+console.log('# Update Absensi'); // for title
+console.log('#--------------------'); // for title
+var updateSiswa = updateAbsensiSiswa('Senin, 05-11-2018', '1B001', 'S');
+console.log(updateSiswa);
